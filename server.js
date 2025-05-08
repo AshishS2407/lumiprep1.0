96d337f5f87ac2e1041f6bb65a513e6ac9e871d7
Ashish S <ashishanandhu78@gmail.com>
2025-05-08 21:19:17 +0530

    before cors error

--- server.js
+++ server.js
@@ -10,22 +10,13 @@ connectDB();
 
 const app = express();
 
-const corsOptions = {
-  origin: ["http://localhost:5173", "https://lumiprep-vert.vercel.app"],
+
+
+app.use(cors({
+  origin: ["http://localhost:5173", "https://lumiprep.vercel.app"],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
+}));
 
-app.use(cors(corsOptions));
-
-app.options('*', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.sendStatus(204);
-});
 
 app.use(express.json());
 
@@ -33,7 +24,7 @@ app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/tests', testRoutes);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
