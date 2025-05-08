9d472e476ce945ebbad9a4b0ea0b819ea92e9017
Ashish S <ashishanandhu78@gmail.com>
2025-05-08 20:46:34 +0530

    corss

--- server.js
+++ server.js
@@ -10,14 +10,22 @@ connectDB();
 
 const app = express();
 
-
-app.options('*', cors());
-
-app.use(cors({
-  origin: ["http://localhost:5173", "https://lumiprep.vercel.app", "https://lumiprep-vert.vercel.app"],
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://lumiprep-vert.vercel.app"],
   credentials: true,
-}));
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
+app.use(cors(corsOptions));
+
+app.options('*', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.sendStatus(204);
+});
 
 app.use(express.json());
 
@@ -25,7 +33,7 @@ app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/tests', testRoutes);
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
